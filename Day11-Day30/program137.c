f156d42ba6a2317b6f5d18ad9fa513c844065167
Maheeeeeeeeeeeee <maheshboke2003@gmail.com>
2025-12-26 19:42:10 +0530

    Add files via upload

---
+++ Day11-Day30/program137.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef int * IPTR;
+
+bool LinearSearch(int Arr[], int iSize, int iNo)
+{
+    int iCnt = 0, iCount = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            iCount++;
+        }
+    }
+    if(iCount > 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iLength = 0, iCnt = 0, iValue = 0;
+    bool bRet  = 0;
+    IPTR iPtr = NULL;
+
+    printf("Enter the number of elements : ");
+    scanf("%d",&iLength);
+
+    //Step 1: Allocate the memory
+    iPtr = (IPTR)malloc(iLength * sizeof(int));
+
+    if(NULL == iPtr)
+    {
+        printf("Unable to allocate the memory");
+        return -1;
+    }
+
+    printf("Enter the values : \n");
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&iPtr[iCnt]);
+    }
+
+    printf("Enter the number to search : \n");
+    scanf("%d",&iValue);
+
+    //Step 2: Use the memory
+    bRet = LinearSearch(iPtr, iLength, iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is present in the data", iValue);
+    }
+    else
+    {
+        printf("%d is absent in the data", iValue);
+    }
+
+    //Step 3: Free the memory
+    free(iPtr);
+
+    return 0;
+}
